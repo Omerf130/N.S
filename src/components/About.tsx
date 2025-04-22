b30bd06005d4330f77dc88ba1d33e4fa603e9848
Omerf130 <ofaclieru@gmail.com>
2025-04-22 18:58:52 +0300

    css changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,55 +8,58 @@ export default function About() {
         py: 8,
         px: 6,
         display: "flex",
-        gap: 4,
         "@media (max-width:750px)": {
           flexDirection: "column",
         },
-        alignItems: "center",
         position: "relative",
-        zIndex:2,
+        zIndex: 2,
       }}
     >
-      <Box
+      <Container
         sx={{
-          backgroundImage: 'url("/pics/about.jpeg")',
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          flexShrink: 0,
-          borderRadius: "10px",
-          width: "calc(35vw - 4px)",
-          height: "calc(50vw - 6px)",
+          display: "flex",
+          gap: 4,
+          alignItems: "center",
           "@media (max-width:750px)": {
-            width: "calc(120% - 16px)",
-            height: "calc(110vw - 8px)",
+            flexDirection: "column",
           },
-          // width: {
-          //   xs: "100%",
-          //   sm: "calc(40% - 16px)",
-          // },
-          // height: {
-          //   xs: "50vw", // 50% of viewport width on mobile
-          //   sm: "60vh", // 60% of viewport height on larger screens
-          // },
-          maxWidth: 500,
         }}
-      />
-      <Container sx={{ maxWidth: "sm" }}>
-        <Typography variant="h4" gutterBottom>
-          מי אני?
-        </Typography>
-        <Typography variant="h5">
-          ני נזי שרון, מעצבת פנים עם תשוקה אמיתית לאומנות עיצוב החללים, ובפרט
-          בעיצוב מטבחים. עם ראש פתוח וייחודי, אני מאמינה כי כל אתגר הוא הזדמנות
-          לחדש וליצור משהו מרגש. אני שואפת לחצות גבולות ולשבור מיתוסים מקובלים
-          בתחום העיצוב, ולממש את החלומות של לקוחותיי בעזרת פתרונות מותאמים
-          אישית. הגישה שלי היא יסודית וממוקדת, ואני משקיעה את הלב והנשמה בכל
-          פרויקט. אני מביאה איתי חשיבה מחוץ לקופסה, ומקשיבה לצרכים ולרצונות של
-          לקוחותיי כדי לשלב אותם עם החזון שלי. כך אני יוצרת מרחבים שמשקפים את
-          האישיות והסגנון של כל לקוח. עיצוב המטבח הוא תחום שמיוחד לי, ואני מתמחה
-          ביצירת חללים פונקציונליים ויפהפיים, המשלבים בין נוחות לאסתטיקה. ובסופו
-          של דבר, בואו יחד נהפוך מדמיון… למציאות.
-        </Typography>
+      >
+        <Box
+          sx={{
+            backgroundImage: 'url("/pics/about.jpeg")',
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            flexShrink: 0,
+            borderRadius: "10px",
+            width: "calc(35vw - 4px)",
+            height: "calc(50vw - 6px)",
+            "@media (max-width:750px)": {
+              width: "calc(120% - 16px)",
+              height: "calc(110vw - 8px)",
+            },
+
+            maxWidth: 500,
+            maxHeight: 650,
+          }}
+        />
+        <Container sx={{ maxWidth: "sm" }}>
+          <Typography variant="h4" gutterBottom>
+            מי אני?
+          </Typography>
+          <Typography variant="h5">
+            ני נזי שרון, מעצבת פנים עם תשוקה אמיתית לאומנות עיצוב החללים, ובפרט
+            בעיצוב מטבחים. עם ראש פתוח וייחודי, אני מאמינה כי כל אתגר הוא
+            הזדמנות לחדש וליצור משהו מרגש. אני שואפת לחצות גבולות ולשבור מיתוסים
+            מקובלים בתחום העיצוב, ולממש את החלומות של לקוחותיי בעזרת פתרונות
+            מותאמים אישית. הגישה שלי היא יסודית וממוקדת, ואני משקיעה את הלב
+            והנשמה בכל פרויקט. אני מביאה איתי חשיבה מחוץ לקופסה, ומקשיבה לצרכים
+            ולרצונות של לקוחותיי כדי לשלב אותם עם החזון שלי. כך אני יוצרת מרחבים
+            שמשקפים את האישיות והסגנון של כל לקוח. עיצוב המטבח הוא תחום שמיוחד
+            לי, ואני מתמחה ביצירת חללים פונקציונליים ויפהפיים, המשלבים בין נוחות
+            לאסתטיקה. ובסופו של דבר, בואו יחד נהפוך מדמיון… למציאות.
+          </Typography>
+        </Container>
       </Container>
     </Box>
   );
--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -1,15 +1,64 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Paper, Typography } from "@mui/material";
+
+const data = [
+  {
+    title: "קבלת לוח השראה",
+    desc: "בו מוצג החומרים,הגוונים והרהיטים להגשמת חלומכם.",
+    id: "1",
+  },
+  {
+    title: "הדמיה עבור החלל הנבחר",
+    desc: "על מנת שתוכלו לראות כיצד הכל הולך להיראות.",
+    id: "2",
+  },
+  {
+    title: "חוברת תוכניות (הרלוונטי לפרויקט)",
+    desc: "תוכנית רהיטים, תוכנית ריצוף,תוכנית גבס, תוכנית חשמל, תוכנית תאורה,תוכנית חדר רחצה, תוכנית נגרות ומטבח…",
+    id: "3",
+  },
+];
 
 export default function Plans() {
   return (
-    <Box id="plans" sx={{ py: 8 }}>
-      <Container>
-        <Typography variant="h4" gutterBottom>
-          Plans
-        </Typography>
-        <Typography>
-          This is a simple about section. You can customize it with more content or imagery.
-        </Typography>
+    <Box id="plans" sx={{ py: 8, position: "relative", zIndex: 2 }}>
+      <Container sx={{display:"flex", flexDirection: "column", gap:"10px"}}>
+        <Typography variant="h3">המסע שלנו ביחד:</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 4,
+            alignItems: "stretch",
+            "@media (max-width:750px)": {
+              flexDirection: "column",
+            },
+          }}
+        >
+          {data.map((item) => (
+            <Box
+              key={item.id}
+              sx={{ flex: 1, display: "flex", alignItems: "stretch" }}
+            >
+              <Paper
+                elevation={3}
+                sx={{
+                  padding: 3,
+                  borderRadius: 3,
+                  width: "100%",
+                  textAlign: "center",
+                  transition: "transform 0.3s ease",
+                  "&:hover": {
+                    transform: "scale(1.03)",
+                  },
+                }}
+              >
+                <Typography variant="h5" gutterBottom>
+                  {item.title}
+                </Typography>
+                <Typography variant="body1">{item.desc}</Typography>
+              </Paper>
+            </Box>
+          ))}
+        </Box>
       </Container>
     </Box>
   );
