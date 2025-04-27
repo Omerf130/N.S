19ff127d95519e65c66f780d6090c7ff368088b7
Omerf130 <ofaclieru@gmail.com>
2025-04-27 14:33:39 +0300

    contact added

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Container, TextField, Button, Box, Typography } from "@mui/material";
 import { useMediaMatch } from "../hooks/useMediaMatch";
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
